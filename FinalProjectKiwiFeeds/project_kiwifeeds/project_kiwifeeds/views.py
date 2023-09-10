982477f083373e94869142054a6b89ccb4ed882b
vijend795 <107410066+vijend795@users.noreply.github.com>
2023-09-10 12:57:21 +1200

    upload Django Project with setup Instructions
    upload Django Project with setup Instructions
    tailwind and django debug toolbar is not set up yet

---
+++ FinalProjectKiwiFeeds/project_kiwifeeds/project_kiwifeeds/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+# def home(request):
+    
+#     return render(request,'index.html')
+#     # return HttpResponse("Hello, World!")
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'index.html')
