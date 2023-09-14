25d2026a77518a399ad82fe36264a69953a1ad77
A <alyssa@kvasl.com>
2023-09-14 12:45:10 +1200

    Added fields to views and templates

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_user/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_user/views.py
@@ -1,25 +1,46 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from models import UserProfile
 
-class UserProfileView(DeleteView):
-    pass
+class UserProfileView(DetailView):
+    model = UserProfile
+    fields = ['profile_picture', 'phone_number']
+    template_name = 'user_profile_page.html'
 
 class UserUpdateView(UpdateView):
-    pass
+    model = UserProfile
+    fields = ['profile_picture', 'phone_number']
+    success_url = reverse_lazy('profile-page')
+    template_name = 'user_profile_edit.html'
+
 
 class UserCreateView(CreateView):
-    pass
+    model = UserProfile
+    fields = ['profile_picture', 'phone_number']
+    success_url = reverse_lazy('profile-page')
+    template_name = 'user_signup.html'
 
+
+#Will need to create template page for succesful deletion or redir to homepage
 class UserDeleteView(DeleteView):
-    pass
+    model = UserProfile
+    success_url = reverse_lazy('profile-deleted')
+    template_name = 'user_profile_delete.html'
 
-#Login and Logout Using Django auth
+# #Views Using Django auth
 
 class UserLoginView(LoginView):
-    pass
+    success_url = reverse_lazy('home-page')
+    template_name = 'user_login.html'
 
 class UserLogoutView(LogoutView):
-    pass
+    success_url = reverse_lazy('home-page')
+    template_name = 'user_logout.html'
+
+# class UserChangePassword(PasswordChangeView):
+#     template_name = 'user_profilePage.html'
+
+# class UserPasswordChangeDone(PasswordChangeDoneView):
+#     template_name = 'user_profilePage.html'
