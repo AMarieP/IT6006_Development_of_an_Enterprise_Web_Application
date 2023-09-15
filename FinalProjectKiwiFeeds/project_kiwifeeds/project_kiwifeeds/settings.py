3844d856625c1517904478a3af0428e9ce3cb0c5
vijend795 <107410066+vijend795@users.noreply.github.com>
2023-09-15 14:51:29 +1200

    updated NPM_BIN_PATH=r"/usr/local/bin/npm oath for mac book
    updated NPM_BIN_PATH=r"/usr/local/bin/npm oath for mac book

--- FinalProjectKiwiFeeds/project_kiwifeeds/project_kiwifeeds/settings.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/project_kiwifeeds/settings.py
@@ -145,6 +145,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 NPM_BIN_PATH=r"C:\Program Files\nodejs\npm.cmd"
+# NPM_BIN_PATH=r"/usr/local/bin/npm"
+
 TAILWIND_APP_NAME = 'theme'
 
 INTERNAL_IPS = [
