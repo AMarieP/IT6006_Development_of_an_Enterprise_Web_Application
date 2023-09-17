8a88f3cf3cdf36f3cd8fc2647b8b9146c63b2347
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 11:58:08 +1200

    Added path for restaurant details
    A page for individual restaurant

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/urls.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
    #path("",review_list ,name="home"),
    #path("reviews/", review_list, name="reviewlist"),
+   path("restaurants/", restaurant_list, name="restaurant-list"),
+   path("restaurant/<int:restaurant_id>/", restaurant_details, name="restaurant-details"),
    path("reviews/", review_list, name="review-list"),
    path("review/<int:review_id>/", review_details, name="review-details"),
-   path("restaurants/", restaurant_list, name="restaurants-list")
 ]
