8a19867895b4be558491c95d2cbe70bb54ae99ec
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 03:52:45 +1200

    Updated views.py
    Linked with model to find the review list

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from app_review.models import Review
+
 # Create your views here.
 def review_list(request):
-    return HttpResponse("Hello Reviews")
-    #return render(request,'app_review/index.html',{})
+    model=Review.objects.get();
+    #return HttpResponse("Hello Reviews")
+    return render(request,'app_review/review_list.html',{review_list : model})
