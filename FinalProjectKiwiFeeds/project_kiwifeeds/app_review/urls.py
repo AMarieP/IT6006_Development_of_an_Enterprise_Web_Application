93d5d62933ab7df070ab32ccafcff3476b00b7be
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 11:32:19 +1200

    Added restaurant list

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/urls.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import review_list, review_details
+from .views import restaurant_list, review_list, review_details
 from . import views
 
 urlpatterns = [
    #path("",review_list ,name="home"),
    #path("reviews/", review_list, name="reviewlist"),
-   path("reviews/", views.review_list, name="review-list"),
+   path("reviews/", review_list, name="review-list"),
    path("review/<int:review_id>/", review_details, name="review-details"),
-   path("restaurants/", restaurant_list, name="Restaurants")
+   path("restaurants/", restaurant_list, name="restaurants-list")
 ]
--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from app_review.models import Review
+from app_review.models import Restaurant, Review
 
 # Create your views here.
 def review_list(request):
     model=Review.objects.all()
     return render(request,'app_review/review_list.html',{"review_list" : model})
 def review_details(request, review_id):
-    return HttpResponse("Review Details")
-    #model=Tour.objects.get(pk=tour_id)
-    #return render(request, 'tours/tourdetails.html',{"tour": model})
+    model=Review.objects.get(pk=review_id)
+    return render(request, 'app_review/review_details.html',{"review": model})
+def restaurant_list(request):
+    model=Restaurant.objects.all()
+    return render(request,'app_review/restaurant_list.html',{"restaurant_list" : model})
