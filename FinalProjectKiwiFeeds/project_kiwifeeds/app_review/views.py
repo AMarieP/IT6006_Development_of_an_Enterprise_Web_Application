41d13ef8954cd17f7a67605a2d335b7cbffb6cca
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 02:49:06 +1200

    Created Views for Review App

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def review_list(request):
+    return HttpResponse("Hello Reviews")
+    #return render(request,'app_review/index.html',{})
