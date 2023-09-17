5c5bdfebbf3f2c49d7fcf9d2463eb90a6806db4a
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 11:45:49 +1200

    Modified app name in admin and views

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/admin.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app_review.models import Restaurant, Review
+from app_restaurants.models import Restaurant, Review
 
 # Register your models here.
 admin.site.register(Review)
