2eb4caba2de2ed2c84b2cb043b6a5ae49deab4db
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 04:26:13 +1200

    Fixed pathlist

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/urls.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from views import review_list
 
 urlpatterns = [
-   path("",review_list ,name="reviewlist")
+   path("",review_list ,name="home"),
+   path("reviews/", review_list, name="reviewlist"),
+   path("review/<int:review_id>", review_list, name="review-details")
+
 ]
