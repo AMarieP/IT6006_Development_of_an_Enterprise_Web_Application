686aa1a8735dfc308958747e4da41872b009ef1c
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-18 06:12:51 +1200

    Created restaurant list

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_review/views.py
@@ -5,6 +5,9 @@ from app_review.models import Review
 
 # Create your views here.
 def review_list(request):
-    model=Review.objects.get();
-    #return HttpResponse("Hello Reviews")
-    return render(request,'app_review/review_list.html',{review_list : model})
+    model=Review.objects.all()
+    return render(request,'app_review/review_list.html',{"review_list" : model})
+def review_details(request, review_id):
+    return HttpResponse("Review Details")
+    #model=Tour.objects.get(pk=tour_id)
+    #return render(request, 'tours/tourdetails.html',{"tour": model})
