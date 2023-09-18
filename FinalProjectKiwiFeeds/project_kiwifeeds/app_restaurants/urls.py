91982e571baaaeb335f3bd0a004d9cc1cc7567d3
kbbs1 <ksua211@mywhitecliffe.com>
2023-09-19 05:42:11 +1200

    Created new URL to delete restaurant

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/urls.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_restaurants/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import restaurant_list, restaurant_details, RestaurantCreateView, RestaurantEditView
+from .views import restaurant_list, restaurant_details, RestaurantCreateView, RestaurantEditView, RestaurantDeleteView
 
 urlpatterns = [
    path("", restaurant_list, name="restaurant-list"),
    path("<int:restaurant_id>/", restaurant_details, name="restaurant-details"),
    path("create/", RestaurantCreateView, name="create-restaurant"),
    path("<int:restaurant_id>/edit/", RestaurantEditView, name="edit-restaurant"),
+   path("<int:restaurant_id>/delete/", RestaurantDeleteView, name="delete-restaurant"),
 #    path("reviews/", review_list, name="review-list"),
 #    path("review/<int:review_id>/", review_details, name="review-details"),
 ]
