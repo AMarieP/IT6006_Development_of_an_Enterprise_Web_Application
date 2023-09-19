d1a7fa0c316b4b70d192d8ffad885cad9a697998
20220534 <johnearllibradilla@gmail.com>
2023-09-19 14:43:12 +1200

    created models created views

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/models.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+ #   user = models.ForeignKey()
+ #  restaurant = models.ForeignKey()
+    rating = models.IntegerField()
+    review = models.TextField()
--- FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView,DetailView,ListView,UpdateView
+from .models import Review
 # Create your views here.
+
+class CreateReview(CreateView):
+    model = Review
+
+class ReviewDetail(DetailView):
+    model = Review
+
+class ReviewList(ListView):
+    model = Review
+
+class EditReview(UpdateView):  
+    model = Review
+
