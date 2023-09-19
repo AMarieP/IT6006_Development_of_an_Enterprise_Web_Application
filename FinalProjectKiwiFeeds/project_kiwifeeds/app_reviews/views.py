0083aa77be3f477caab0ce806775967c7948c10e
20220534 <johnearllibradilla@gmail.com>
2023-09-19 15:02:21 +1200

    Created templates on views

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/views.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/views.py
@@ -5,13 +5,16 @@ from .models import Review
 
 class CreateReview(CreateView):
     model = Review
+    template_name ="app_reviews/reviews_createreview.html"
 
 class ReviewDetail(DetailView):
     model = Review
+    template_name ="app_reviews/reviews_editreview.html"
 
 class ReviewList(ListView):
     model = Review
+    template_name = "app_reviews/reviews_reviewdetail.html"
 
 class EditReview(UpdateView):  
     model = Review
-
+    template_name =  "app_reviews/reviews_reviewlist.html"
