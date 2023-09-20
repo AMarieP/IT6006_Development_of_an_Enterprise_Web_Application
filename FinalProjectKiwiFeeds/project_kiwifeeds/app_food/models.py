136cf3e8935b29352e0156683d4b04c56078420d
A <alyssa@kvasl.com>
2023-09-20 23:57:12 +1200

    Refactor: Spelling mistake

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_food/models.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_food/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=255)
-    discription = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
     food_picture = models.ImageField(upload_to='profile_pics', height_field=None, width_field=None)
     food_price = models.DecimalField(max_digits=5, decimal_places=2)
     restaurant = models.ForeignKey('app_restaurants.Restaurant', on_delete=models.CASCADE)
