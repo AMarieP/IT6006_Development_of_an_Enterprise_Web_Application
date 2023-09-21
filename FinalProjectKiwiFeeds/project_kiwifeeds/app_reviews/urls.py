6b082b3df612e64d951a54ef2c9dcee97a38b19c
A <alyssa@kvasl.com>
2023-09-21 17:00:28 +1200

    Updated review urls to reflect removed views

--- FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/urls.py
+++ FinalProjectKiwiFeeds/project_kiwifeeds/app_reviews/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CreateReview, ReviewDetail, ReviewList, EditReview
+from .views import CreateReview, ReviewList
 
 urlpatterns = [
     path("write-a-review/", CreateReview.as_view(), name='write-a-review'),
-    path("<int:pk>/review/", ReviewDetail.as_view(), name='review'),
     path("reviews/", ReviewList.as_view(), name='review-list'),
-    path("/<int:pk>/edit-review/", EditReview.as_view(), name='edit-review'),
+    # path("<int:pk>/review/", ReviewDetail.as_view(), name='review'),
+    # path("/<int:pk>/edit-review/", EditReview.as_view(), name='edit-review'),
 
 ]
